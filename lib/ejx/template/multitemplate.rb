843db51116b90923e014e79220c793f12be00bd1
Ben Ehmke <benehmke@gmail.com>
2021-12-09 10:50:21 -0600

    fix multitemplate with assigned vars

--- lib/ejx/template/multitemplate.rb
+++ lib/ejx/template/multitemplate.rb
@@ -1,9 +1,16 @@
 class EJX::Template::Multitemplate < EJX::Template::Subtemplate
 
   def to_js(indentation: 4, var_generator: nil, append: "__output")
-    output = "#{' '*indentation}__ejx_append("
-    output << @children.first << "\n"
-
+    already_assigned = @children.first =~ /\A\s*(var|const|let)\s+(\S+)/
+    output_var = $2
+    output = ""
+    if already_assigned# || !@append
+      output << "#{' '*indentation}#{@children.first}\n"
+    else
+      output << "#{' '*indentation}__ejx_append("
+      output << @children.first << "\n"
+    end
+    
     @children[1..-2].each do |child|
       output << case child
       when EJX::Template::String
@@ -16,9 +23,13 @@ class EJX::Template::Multitemplate < EJX::Template::Subtemplate
         child.to_js(indentation: indentation + 4, var_generator: var_generator, append: append)
       end
     end
-
+    
     output << ' '*indentation << @children.last.strip.delete_suffix(';')
-    output << ", #{append}, true, __promises);\n"
+    if already_assigned
+      output << ";\n#{' '*indentation}__ejx_append(#{output_var}, #{append}, true, __promises);\n"
+    else
+      output << ", #{append}, true, __promises);\n"
+    end
 
     output
   end
